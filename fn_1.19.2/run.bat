2ce03b2bb8bf67da32b14674a83f617e7013d04e
ExplodingImplosion <60752903+ExplodingImplosion@users.noreply.github.com>
2024-07-01 16:40:48 -0400

    %datenow%

--- fn_1.19.2/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-"C:\Program Files\Java\jdk-18.0.2.1\bin\java.exe" -Dlog4j2.formatMsgNoLookups=true @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.19.2-43.3.5/win_args.txt
-
-echo Press CTRL+C to cancel GitHub commit.
-
-timeout 3
